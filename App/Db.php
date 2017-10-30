918fdee28c7051ac4ffc2b500799ad563da82e34
Eugene Solonenko <8661319@mail.ru>
2017-10-30 20:18:47 +0500

    First commit

--- App/Db.php
+++ App/Db.php
@@ -1,12 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Администратор
- * Date: 30.10.2017
- * Time: 16:33
- */
 
 class Db
 {
-p[]
+    protected $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = new \PDO('mysql:host=localhost;dbname=php2', 'root', '');
+    }
+
+    public function query(string $sql, $params = [], $class = \stdClass::class)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($params);
+        return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
+    }
 }
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/autoload.php';
+$db = new \App\Db();
+var_dump(
+    $db->query('SELECT * FROM news WHERE id=:id', [':id'=>1])
+);
