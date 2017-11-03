1204d653d5e0fbba174abf2ee6d1ba6d690deb7b
Eugene Solonenko <8661319@mail.ru>
2017-11-03 14:14:10 +0500

    First commit

--- tests/testDb.php
+++ tests/testDb.php
@@ -1,8 +1,16 @@
 <?php
 require __DIR__ . '/../autoload.php';
+
 use App\Db;
 
 $db = new Db();
-/** тест метода Execute() ---*/
-$query= 'INSERT INTO test (id, test_field) VALUES (:id,:test_field)';
+
+/* тест метода execute() */
+
+$query = 'INSERT INTO test (id, test_field) VALUES (:id,:test_field)';
 assert(true === $db->execute($query, [':id' => null, ':test_field' => 'Test string']));
+
+$query = 'UPDATE test SET test_field = :text WHERE id = :id';
+assert(true === $db->execute($query, [':text' => 'New Test string', ':id' => 4]));
+
+
