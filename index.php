70e257ac8f22d4e49a26793f17ff7404040b7e48
Eugene Solonenko <8661319@mail.ru>
2017-12-07 13:16:43 +0500

    First commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require __DIR__ . '/autoload.php';
-$data = \App\Models\Article::findById(3);
-
+$data = \App\Models\Article::findLatest(3);
+//$data = \App\Models\Article::findById(3);
 include __DIR__ . '/Templates/index.php';
