863bd49bec3929fcbc7b8010a23d701c99aeaf48
Eugene Solonenko <8661319@mail.ru>
2017-12-18 15:08:24 +0500

    +updates

--- templates/admin/articleForm.php
+++ templates/admin/articleForm.php
@@ -10,6 +10,7 @@
 <body>
 <hr>
 <form action="/App/Admin/save.php" method="post">
+
     <h1>
         <?php if (!empty($article->id)) : ?>
             Редактирование новости
@@ -21,10 +22,8 @@
 
     <p><label for="title"><strong>Заголовок:</strong></label></p>
     <input type="text" name="title" id="title" class="title" value="<?php echo $article->title ?? null ?>">
-
     <p><label for="text"><strong>Текст:</strong></label></p>
     <textarea name="text" id="text" class="text"><?php echo $article->text ?? null ?></textarea>
-
     <hr>
     <input type="submit" value="Сохранить" class="button">
     <a href="/App/Admin/index.php" class="button">Отмена</a>
