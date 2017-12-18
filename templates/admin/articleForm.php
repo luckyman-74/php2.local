7f4b43ac7cd455e5d5c1d0c3be092b75e98616d4
Eugene Solonenko <8661319@mail.ru>
2017-12-18 15:07:44 +0500

    +updates

--- templates/admin/articleForm.php
+++ templates/admin/articleForm.php
@@ -8,26 +8,22 @@
 </head>
 
 <body>
-
-<h1>
-    <?php if (!empty($article->id)) : ?>
-        Редактирование новости
-    <?php else: ?>
-        Добавление новости
-    <?php endif; ?>
-</h1>
 <hr>
 <form action="/App/Admin/save.php" method="post">
-
-    <?php if (!empty($article->id)) : ?>
-        <input type="hidden" name="id" value="<?php echo $article->id ?>">
-    <?php endif; ?>
+    <h1>
+        <?php if (!empty($article->id)) : ?>
+            Редактирование новости
+            <input type="hidden" name="id" value="<?php echo $article->id ?>">
+        <?php else: ?>
+            Добавление новости
+        <?php endif; ?>
+    </h1>
 
     <p><label for="title"><strong>Заголовок:</strong></label></p>
     <input type="text" name="title" id="title" class="title" value="<?php echo $article->title ?? null ?>">
 
     <p><label for="text"><strong>Текст:</strong></label></p>
-    <textarea  name="text" id="text" class="text" ><?php echo $article->text ?? null ?></textarea>
+    <textarea name="text" id="text" class="text"><?php echo $article->text ?? null ?></textarea>
 
     <hr>
     <input type="submit" value="Сохранить" class="button">
