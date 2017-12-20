a56386a14285702b86efe9a933250f7a7d2136c7
Eugene Solonenko <8661319@mail.ru>
2017-12-20 09:09:40 +0500

    +updates

--- templates/admin/createUpdate.php
+++ templates/admin/createUpdate.php
@@ -21,9 +21,9 @@
     </h1>
 
     <p><label for="title"><strong>Заголовок:</strong></label></p>
-    <input type="text" name="title" id="title" class="title" value="<?php echo $article->title ?? null ?>">
+    <input type="text" name="title" id="title" class="title" required value="<?php echo $article->title ?? null ?>">
     <p><label for="text"><strong>Текст:</strong></label></p>
-    <textarea name="text" id="text" class="text"><?php echo $article->text ?? null ?></textarea>
+    <textarea name="text" id="text" class="text" required><?php echo $article->text ?? null ?></textarea>
     <hr>
     <input type="submit" value="Сохранить" class="button">
     <a href="/App/Admin/index.php" class="button">Отмена</a>
