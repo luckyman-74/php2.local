eacd2d40836db461c37b0b1663bfc564ed4d94eb
Eugene Solonenko <8661319@mail.ru>
2017-12-20 13:46:07 +0500

    Работа над ошибками

--- App/Admin/createUpdate.php
+++ App/Admin/createUpdate.php
@@ -1,5 +1,5 @@
 <?php
 require __DIR__ . '/../../autoload.php';
 
-$article = !empty($_GET['id']) ?  \App\Models\Article::findById($_GET['id']) : null;
+$article = !empty($_GET['id']) ? \App\Models\Article::findById($_GET['id']) : null;
 include __DIR__ . '/../../templates/admin/createUpdate.php';
