f3b9d7f8a90f0865ff538858dabcbce8c3c147ec
Eugene Solonenko <8661319@mail.ru>
2017-12-20 08:56:51 +0500

    +updates

--- App/Admin/createUpdate.php
+++ App/Admin/createUpdate.php
@@ -2,4 +2,4 @@
 require __DIR__ . '/../../autoload.php';
 
 !empty($_GET['id']) ? $article = \App\Models\Article::findById($_GET['id']) : null;
-include __DIR__ . '/../../templates/admin/articleForm.php';
+include __DIR__ . '/../../templates/admin/createUpdate.php';
