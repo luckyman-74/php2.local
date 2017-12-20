9d8bd36542b523181dfa6a1ca4d55e46be17c1a0
Eugene Solonenko <8661319@mail.ru>
2017-12-20 14:27:39 +0500

    Работа над ошибками

--- App/Admin/save.php
+++ App/Admin/save.php
@@ -2,17 +2,14 @@
 require __DIR__ . '/../../autoload.php';
 
 if (!empty($_POST['id'])) {
-    if (null !== $article) {
-        $article = \App\Models\Article::findById($_POST['id']);
-    } else {
+    if (!$article = \App\Models\Article::findById($_POST['id'])) {
         exit ('Не существует записи с id: ' . $_POST['id']);
     }
-}else {
-$article = new \App\Models\Article();
-
-    $article->title = $_POST['title'];
-    $article->text = $_POST['text'];
-    $article->save();
-    header('location: /App/Admin/index.php');
-    exit;
+} else {
+    $article = new \App\Models\Article();
 }
+$article->title = $_POST['title'];
+$article->text = $_POST['text'];
+$article->save();
+header('location: /App/Admin/index.php');
+exit;
