f6b2d1cc5b9541bffe4f575ed20e2d576bf47d1d
Eugene Solonenko <8661319@mail.ru>
2017-12-20 13:33:44 +0500

    Работа над ошибками

--- App/Admin/save.php
+++ App/Admin/save.php
@@ -1,12 +1,13 @@
 <?php
 require __DIR__ . '/../../autoload.php';
 
-if (!empty($_POST)) {
+if (!empty($_POST['id'])) {
+    $article = \App\Models\Article::findById($_POST['id']);
+} else {
     $article = new \App\Models\Article();
-    $article->title = $_POST['title'];
-    $article->text = $_POST['text'];
-    isset($_POST['id']) ? $article->id = $_POST['id'] : null;
-    $article->save();
-    header('location: /App/Admin/index.php');
-    exit;
 }
+$article->title = $_POST['title'];
+$article->text = $_POST['text'];
+$article->save();
+header('location: /App/Admin/index.php');
+exit;
