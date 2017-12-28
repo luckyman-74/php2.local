44179d5ae9e6965e31f9b8217adac83aaa5ee8f1
Eugene Solonenko <8661319@mail.ru>
2017-12-28 12:44:43 +0500

    + View class

---
+++ App/View.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use App\Traits\GetSetTrait;
+
+class View
+{
+    use GetSetTrait;
+
+    public function render(string $template): string
+    {
+        ob_start();
+        foreach ($this->data as $key => $value) {
+            $$key = $value;
+        }
+        include $template;
+        return ob_get_clean();
+    }
+
+    public function display(string $template): void
+    {
+      echo $this->render($template);
+    }
+
+}
