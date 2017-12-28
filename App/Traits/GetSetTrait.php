24436a21b90943cd1aa2b976f885b6ec3ccd2711
Eugene Solonenko <8661319@mail.ru>
2017-12-28 12:36:38 +0500

    + GetSetTrait

---
+++ App/Traits/GetSetTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+
+trait GetSetTrait
+{
+    protected $data = [];
+
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->data[$name]);
+    }
+}
